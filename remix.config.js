f658c9dca5fb1f5282481f38f8b82f824d0bc206
Roz <rosalyn.bonilla@podium.com>
2023-11-05 19:02:48 +0000

    add netlify

--- remix.config.js
+++ remix.config.js
@@ -1,5 +1,7 @@
+import { config } from "@netlify/remix-adapter";
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  ...(process.env.NODE_ENV === "production" ? config : undefined),
   ignoredRouteFiles: ["**/.*"],
   postcss: true,
   serverModuleFormat: "cjs",
